0090d8bb8ccd86db3686fe24868846b083b4bdf1
Roman Korolev <r.korolev@portalbilet.ru>
2025-06-13 13:25:58 +0500

    добавил логгирование

--- tg_bot.py
+++ tg_bot.py
@@ -1,13 +1,23 @@
 import asyncio
-import logging
 import sys
 from os import getenv
+import logging
 
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",  # Формат сообщения,
+    handlers=[
+        logging.FileHandler("tg_bot_users.log", encoding="utf-8"),  # Важно: encoding="utf-8"
+        logging.StreamHandler()  # Вывод в консоль
+    ]
+)
+logger = logging.getLogger('TG_BOT')
 
 # Bot token can be obtained via https://t.me/BotFather
 TOKEN = getenv("BOT_TOKEN")
@@ -28,6 +38,13 @@ async def command_start_handler(message: Message) -> None:
     # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
     # method automatically or call API method directly via
     # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    user = message.from_user  # Получаем данные пользователя
+
+    first_name = user.first_name  # Имя (если есть)
+    last_name = user.last_name  # Фамилия (если есть)
+    username = user.username
+    logger.info("start<- %s, %s, %s | Имя, Фамилия, Никнейм", first_name, last_name, username)
+
     await message.answer(f"Привет! Ваш chat id: {html.bold(message.chat.id)}")
 
 
